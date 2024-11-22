50c8341417b2e56c4d17d6049283cacf67fec414
Wei HUANG <wei.b.huang@capgemini.com>
2024-11-23 00:05:56 +0100

    changer version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ val archivaUserName: String by project
 val archivaPassword: String by project
 
 group = "io.sharptree"
-version = "1.0.2"
+version = "1.0.3"
 
 val vendor = "Sharptree"
 val product = "autoscript-dbc"
 val distro = "autoscript-dbc"
 
-project.version = "1.0.2"
+project.version = "1.0.3"
 
 tasks.compileJava {
     sourceCompatibility = "1.8"
