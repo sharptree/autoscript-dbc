a1087df93646840d5ea1c9c56c1b0795f0be631e
Jason VenHuizen <jason.venhuizen@naviam.io>
2025-11-24 14:49:54 -0800

    Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,13 @@ val archivaUserName: String by project
 val archivaPassword: String by project
 
 group = "io.sharptree"
-version = "1.0.3"
+version = "1.0.4"
 
 val vendor = "Sharptree"
 val product = "autoscript-dbc"
 val distro = "autoscript-dbc"
 
-project.version = "1.0.3"
+project.version = "1.0.4"
 
 
 java{
